5bf6b529a7bfac33fee4ad9ad2bd2ffc268a4f47
Abdulaziz Ali <variant.abdulaziz@gmail.com>
2023-04-18 14:29:18 +0300

    define domain

---
+++ Backend/ChamberOfSecrets/ChamberOfSecrets.CollabChamber.Domain/Participant.cs
@@ -0,0 +1,13 @@
+﻿using ChamberOfSecrets.CollabChamber.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChamberOfSecrets.CollabChamber.Domain;
+
+public class Participant : BaseDomainEntity
+{
+}
+
